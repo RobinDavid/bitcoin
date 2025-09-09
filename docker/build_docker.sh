0ad4670c0f09c0afcb3ad66818417303ae33a013
Nicolas Surbayrole <nsurbayrole@quarkslab.com>
2025-09-09 16:11:54 +0200

    Add docker for protobuf fuzzing

---
+++ docker/build_docker.sh
@@ -0,0 +1,26 @@
+#!/usr/bin/env bash
+
+set -e
+
+BASEDIR=$(cd $(dirname "$0") && pwd -P)
+
+prepare_archive (){
+    pushd "$(git rev-parse --show-toplevel)" >/dev/null
+    git archive -o "${BASEDIR}/btc.tar.gz" --prefix=bitcoin/ HEAD .
+    popd >/dev/null
+}
+
+delete_archive() {
+    rm -f "${BASEDIR}/btc.tar.gz"
+}
+
+prepare_archive
+
+DOCKER_TAG="btc_fuzz:test"
+DOCKERFILE="${BASEDIR}/Dockerfile"
+
+prepare_archive
+
+docker build "${BASEDIR}" -t "${DOCKER_TAG}" -f "${DOCKERFILE}"
+
+delete_archive
